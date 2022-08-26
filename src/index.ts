7d87bc0c8f36acd06ff35c378a8d9b5848904c53
Marvin Reichmann <mreichmann@infonline.de>
2022-08-26 20:13:29 +0200

    fix(package): fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const IoButton = require('./io-button/IoButton.js')
-const IoSelect = require('./io-button/IoSelect.js')
-const IoInput = require('./io-button/IoInput.js')
+const IoSelect = require('./io-select/IoSelect.js')
+const IoInput = require('./io-input/IoInput.js')
 const IoButtonReact = require('./io-button/io-button-react.js')
 
 module.exports = {

c86aaadce57d3ba2ebc62fa473c71656eeb159cf
Marvin Reichmann <mreichmann@infonline.de>
2022-08-26 16:59:23 +0200

    style(lint): added missing semicolon

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { IoButton } from './io-button/IoButton.js';
 export { IoSelect } from './io-select/IoSelect.js';
 export { IoInput } from './io-input/IoInput.js';
 
-export { IoButtonReact } from './io-button/io-button-react.js'
+export { IoButtonReact } from './io-button/io-button-react.js';

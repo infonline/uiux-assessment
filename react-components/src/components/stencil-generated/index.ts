6f31085cc25845e582dfea9907a32399163c0df4
Marvin Reichmann <mreichmann@infonline.de>
2022-08-27 22:17:58 +0200

    chore(package): checked in proper react-components directory

---
+++ react-components/src/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@io/web-components';
+
+import { defineCustomElements } from '@io/web-components/loader';
+
+defineCustomElements();
+export const IoButton = /*@__PURE__*/createReactComponent<JSX.IoButton, HTMLIoButtonElement>('io-button');
+export const IoInput = /*@__PURE__*/createReactComponent<JSX.IoInput, HTMLIoInputElement>('io-input');
